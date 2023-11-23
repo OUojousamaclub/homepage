c4adc19186ab5f49e8d4310a29bf9b0d93532669
Maltonn <ymatsubayashi09@outlook.jp>
2023-11-23 22:21:55 +0900

    update

--- docs/internal/anonymous/main.js
+++ docs/internal/anonymous/main.js
@@ -1,13 +1,15 @@
-submit.addEventListener('click', function() {
-    url=""
-    if (serverSelect.value=="作業部屋"){
-        url='https://discord.com/api/webhooks/1176884868022538292/ySowWiTPAznwI0M8OnjFVEyScB19gNPqEV4hVjYqUVvFseyWF3ZiPCQXXpxa2LVMc83B'
-    }else if(serverSelect.value=="テスト鯖"){
-        url="https://discord.com/api/webhooks/1176879950586974308/8mH-13iYOjgY0PpaHEIi2sfKp21fm9FSRrukyl-O-m2EwvDKhb6DcsW_QGL1S5Cb7Nc1"
+submit.addEventListener('click', function () {
+    url = ""
+    if (serverSelect.value == "作業部屋") {
+        url = 'https://discord.com/api/webhooks/1176884868022538292/ySowWiTPAznwI0M8OnjFVEyScB19gNPqEV4hVjYqUVvFseyWF3ZiPCQXXpxa2LVMc83B'
+    } else if (serverSelect.value == "テスト鯖") {
+        url = "https://discord.com/api/webhooks/1176879950586974308/8mH-13iYOjgY0PpaHEIi2sfKp21fm9FSRrukyl-O-m2EwvDKhb6DcsW_QGL1S5Cb7Nc1"
     }
-    else if(serverSelect.value=="運営"){
-        url="https://discord.com/api/webhooks/1176899917495664742/zjbiVmNUeGTQp7QqVATTyRBR-XqQfxNeY3gpcYDFGywhpp4OADiO2VmXLykt4eYQX6mE"
-    }else{
+    else if (serverSelect.value == "運営1") {
+        url = "https://discord.com/api/webhooks/1176899917495664742/zjbiVmNUeGTQp7QqVATTyRBR-XqQfxNeY3gpcYDFGywhpp4OADiO2VmXLykt4eYQX6mE"
+    } else if (serverSelect.value == "運営2") {
+        url = "https://discord.com/api/webhooks/1177227694618988574/qj3GALEL-ndTpaQw1Nc2SIw_6w1AGBP5klODPBF7GC8Pv9hhnKe2bi4Ff9SbgZeOPS55"
+    } else {
         window.alert("バグですわ！急いで門番を呼んできなさい！")
         return
     }
@@ -23,31 +25,31 @@ submit.addEventListener('click', function() {
             content: msgTextarea.value || '-'
         })
     })
-    .then(res => {
-        console.log(res);
-        msgTextarea.value = '';
-        result.innerText = '無事に送信されましたわ！';
-    }).catch(err => {
-        console.error(err);
-        result.innerText = 'エラーが発生しましたわ...';
-    });
+        .then(res => {
+            console.log(res);
+            msgTextarea.value = '';
+            result.innerText = '無事に送信されましたわ！';
+        }).catch(err => {
+            console.error(err);
+            result.innerText = 'エラーが発生しましたわ...';
+        });
 })
 
 
-randomBtn.addEventListener('click', function() {
-    const randomAttrs=["才能あふれる","寡黙な","冷静沈着な","おてんば","謎めいた","陽気な","物好きな","忠実な",
-    "勤勉な","物静かな","秘密を抱える","孤高の","気まぐれな","怪しい","冷酷な","優雅な","情熱的な","才能溢れる","テンションの高い"];
-    const randomRoles=["お嬢様","執事","執事","メイド","館主","庭師","司書","従者","家政婦","画家","使用人","音楽家",
-    "芸術家","占い師","刺客","舞踏教師","オペラ歌手","芸術家","主の犬"];
+randomBtn.addEventListener('click', function () {
+    const randomAttrs = ["才能あふれる", "寡黙な", "冷静沈着な", "おてんば", "謎めいた", "陽気な", "物好きな", "忠実な",
+        "勤勉な", "物静かな", "秘密を抱える", "孤高の", "気まぐれな", "怪しい", "冷酷な", "優雅な", "情熱的な", "才能溢れる", "テンションの高い"];
+    const randomRoles = ["お嬢様", "執事", "執事", "メイド", "館主", "庭師", "司書", "従者", "家政婦", "画家", "使用人", "音楽家",
+        "芸術家", "占い師", "刺客", "舞踏教師", "オペラ歌手", "芸術家", "主の犬"];
 
-    nameInput.value=randomAttrs[Math.floor(Math.random() * randomAttrs.length)]+randomRoles[Math.floor(Math.random() * randomRoles.length)];
+    nameInput.value = randomAttrs[Math.floor(Math.random() * randomAttrs.length)] + randomRoles[Math.floor(Math.random() * randomRoles.length)];
 });
 
-nameInput.addEventListener('change', function() {
+nameInput.addEventListener('change', function () {
     //save to local storage
     localStorage.setItem('name', nameInput.value);
 });
 
-window.addEventListener('load', function() {
-    nameInput.value=localStorage.getItem('name');
+window.addEventListener('load', function () {
+    nameInput.value = localStorage.getItem('name');
 });
