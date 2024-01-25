92ce7cf4bc2ede87db508ff0ac76109214dbfc6a
Malton <ymatsubayashi09@outlook.jp>
2024-01-25 11:33:17 +0900

    update2

--- docs/internal/vote/result/main.js
+++ docs/internal/vote/result/main.js
@@ -1,18 +1,34 @@
-db.collection("vote").doc("answer")
-    .onSnapshot((doc) => {
-        document.getElementById("result_div").innerHTML = ""
-        dic={}
-        for (key in doc.data()) {
-            val=doc.data()[key]
-            if (dic[val]==undefined){
-                dic[val]=1
-            }else{
-                dic[val]+=1
+const keys=[]
+db.collection('vote').doc('question').get().then((doc) => {
+    document.getElementById("title").innerText = doc.data()["title"]
+    for (key of doc.data()["options"]) {
+        keys.push(key)
+    }
+    F(keys)
+})
+
+F=(keys)=>{
+    db.collection("vote").doc("answer")
+        .onSnapshot((doc) => {
+            document.getElementById("result_div").innerHTML = ""
+            dic={}
+            for (key in doc.data()) {
+                val=doc.data()[key]
+                if (dic[val]==undefined){
+                    dic[val]=1
+                }else{
+                    dic[val]+=1
+                }
             }
-        }
-        for(key in dic){
-            div = document.createElement("div")
-            div.innerText = key + ":" + dic[key]
-            document.getElementById('result_div').appendChild(div)
-        }
-    });
+            for(key of keys){
+                div = document.createElement("div")
+                div.innerText = key + ":" + (dic[key] || 0)
+                document.getElementById('result_div').appendChild(div)
+                
+                bar=document.createElement("div")
+                bar.classList.add("bar")
+                bar.style.width=(dic[key] || 0)*5+"%"
+                document.getElementById('result_div').appendChild(bar)
+            }
+        });
+}
